a3e6db4c1277f01411ef7fcf36236024dcd40f24
kkossally <kkossally@gmail.com>
2019-10-02 17:34:09 -0400

    Complete frontend Auth

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,17 +1,20 @@
 import React from "react";
 import { Route } from 'react-router-dom';
+import AuthRoute from '../util/route_util';
 import GreetingContainer from './greeting/greeting_container';
 import SignupFormContainer from './session_form/signup_form_container';
 import LoginFormContainer from './session_form/login_form_container';
 
 const App = () => (
   <div>
-    <GreetingContainer />
-    <h1>Hoppd</h1>
-    <h3>Drink Like There's No Tomorrow</h3>
+    <header>
+      <GreetingContainer />
+      <h1>Hoppd</h1>
+      <h3>Drink Like There's No Tomorrow</h3>
+    </header>
 
-    <Route path="/login" component={LoginFormContainer} />
-    <Route path="/signup" component={SignupFormContainer} />
+    <AuthRoute exact path="/login" component={LoginFormContainer} />
+    <AuthRoute exact path="/signup" component={SignupFormContainer} />
   </div>
 );
 
